ec97cd37c85d9dd861b127cb4742933817de9d3f
DESKTOP-375QGR3\Eimdadul Haque <eimdadulhaque@gmail.com>
2025-07-10 08:36:26 +0600

    SalesRecord

---
+++ src/STFSA.Domain/Entities/SalesRecord.cs
@@ -0,0 +1,21 @@
+﻿using STFSA.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STFSA.Domain.Entities
+{
+    public class SalesRecord : AuditEntity<int>
+    {
+        public int DatasetId { get; set; }
+        public DateTime Date { get; set; }
+        public string ProductName { get; set; }
+        public string Category { get; set; }
+        public string Region { get; set; }
+        public int Quantity { get; set; }
+        public decimal Revenue { get; set; }
+        public SalesDataset Dataset { get; set; }
+    }
+}
