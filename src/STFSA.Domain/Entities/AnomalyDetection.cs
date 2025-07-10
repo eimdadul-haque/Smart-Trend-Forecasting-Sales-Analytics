2bb07ee970743bf9cb2c878581cb7aa09061eebb
DESKTOP-375QGR3\Eimdadul Haque <eimdadulhaque@gmail.com>
2025-07-10 08:34:30 +0600

    AnomalyDetection

---
+++ src/STFSA.Domain/Entities/AnomalyDetection.cs
@@ -0,0 +1,19 @@
+﻿using STFSA.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STFSA.Domain.Entities
+{
+    public class AnomalyDetection : AuditEntity<int>
+    {
+        public int DatasetId { get; set; }
+        public DateTime AnomalyDate { get; set; }
+        public decimal DetectedValue { get; set; }
+        public decimal ExpectedValue { get; set; }
+        public string Severity { get; set; }
+        public SalesDataset Dataset { get; set; }
+    }
+}
