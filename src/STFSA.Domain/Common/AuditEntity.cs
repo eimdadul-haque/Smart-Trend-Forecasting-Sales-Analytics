abcdb7001b8acf3fc9077952676e7a824be764f4
DESKTOP-375QGR3\Eimdadul Haque <eimdadulhaque@gmail.com>
2025-07-10 08:34:14 +0600

    AuditEntity

---
+++ src/STFSA.Domain/Common/AuditEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STFSA.Domain.Common
+{
+    public class AuditEntity<T>
+    {
+        public required T Id { get; set; }
+        public DateTime? Created { get; set; }
+        public DateTime? Modified { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
