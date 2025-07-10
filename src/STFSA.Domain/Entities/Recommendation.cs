c14ce21fafa910b78189c68d485521d6b87e4de3
DESKTOP-375QGR3\Eimdadul Haque <eimdadulhaque@gmail.com>
2025-07-10 08:35:54 +0600

    Recommendation

---
+++ src/STFSA.Domain/Entities/Recommendation.cs
@@ -0,0 +1,18 @@
+﻿using STFSA.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STFSA.Domain.Entities
+{
+    public class Recommendation : AuditEntity<int>
+    {
+        public int DatasetId { get; set; }
+        public string SuggestedAction { get; set; }
+        public string Reason { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public SalesDataset Dataset { get; set; }
+    }
+}
