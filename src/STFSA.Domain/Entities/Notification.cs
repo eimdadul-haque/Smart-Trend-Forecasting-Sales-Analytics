897603f3286e57d3bb69a93fdd9f05d9ce308f5f
DESKTOP-375QGR3\Eimdadul Haque <eimdadulhaque@gmail.com>
2025-07-10 08:35:31 +0600

    Notification

---
+++ src/STFSA.Domain/Entities/Notification.cs
@@ -0,0 +1,18 @@
+﻿using STFSA.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STFSA.Domain.Entities
+{
+    public class Notification : AuditEntity<int>
+    {
+        public int UserId { get; set; }
+        public string Message { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool IsRead { get; set; }
+        public ApplicationUser User { get; set; }
+    }
+}
