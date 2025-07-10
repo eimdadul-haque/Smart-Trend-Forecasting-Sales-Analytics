664dc0b1e1001e4d37638eba8242f47ce23c0f3a
DESKTOP-375QGR3\Eimdadul Haque <eimdadulhaque@gmail.com>
2025-07-10 08:36:11 +0600

    SalesDataset

---
+++ src/STFSA.Domain/Entities/SalesDataset.cs
@@ -0,0 +1,21 @@
+﻿using STFSA.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STFSA.Domain.Entities
+{
+    public class SalesDataset : AuditEntity<int>
+    {
+        public Guid UserId { get; set; }
+        public string DatasetName { get; set; }
+        public DateTime UploadedAt { get; set; }
+        public string FilePath { get; set; }
+        public List<SalesRecord> SalesRecords { get; set; }
+        public List<ForecastResult> ForecastResults { get; set; }
+        public List<AnomalyDetection> AnomalyDetections { get; set; }
+        public List<Recommendation> Recommendations { get; set; }
+    }
+}
