3a499fc1766fd95bf0c55104a300d29adfb99a58
DESKTOP-375QGR3\Eimdadul Haque <eimdadulhaque@gmail.com>
2025-07-10 08:35:09 +0600

    ForecastResult

---
+++ src/STFSA.Domain/Entities/ForecastResult.cs
@@ -0,0 +1,19 @@
+﻿using STFSA.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STFSA.Domain.Entities
+{
+    public class ForecastResult : AuditEntity<int>
+    {
+        public int DatasetId { get; set; }
+        public DateTime ForecastDate { get; set; }
+        public decimal ForecastValue { get; set; }
+        public string ModelUsed { get; set; }
+        public DateTime GeneratedAt { get; set; }
+        public SalesDataset Dataset { get; set; }
+    }
+}
