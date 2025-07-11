64a59389be5abb1bfd3d8fd68ec7ba365e277177
DESKTOP-375QGR3\Eimdadul Haque <eimdadulhaque@gmail.com>
2025-07-11 19:31:38 +0600

    Profile dto added

---
+++ src/STFSA.Application/User/DTOs/ProfileDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STFSA.Application.User.DTOs
+{
+    public class ProfileDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } 
+
+        public string Email { get; set; } 
+
+        public string PhoneNumber { get; set; } 
+
+        public string Address { get; set; } 
+
+        public string? ProfilePictureUrl { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string? Bio { get; set; }
+
+    }
+}
