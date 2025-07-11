fd5edbbd7e608cd49d4020878b92f6459adc65a1
DESKTOP-375QGR3\Eimdadul Haque <eimdadulhaque@gmail.com>
2025-07-11 18:37:09 +0600

    auth service added

---
+++ src/STFSA.Application/Auth/Interfaces/IAuthService.cs
@@ -0,0 +1,15 @@
+﻿using STFSA.Application.Auth.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STFSA.Application.Auth.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<bool> register(RegisterDto input);
+        Task<string> login(LoginDto input);
+    }
+}
---
+++ src/STFSA.Application/Auth/Services/AuthService.cs
@@ -0,0 +1,27 @@
+﻿using STFSA.Application.Auth.DTOs;
+using STFSA.Application.Auth.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STFSA.Application.Auth.Services
+{
+    public class AuthService : IAuthService
+    {
+        public AuthService()
+        {
+        }
+
+        public async Task<bool> register(RegisterDto input)
+        {
+            return true;
+        }
+
+        public async Task<string> login(LoginDto input)
+        {
+            return "dummy_token";
+        }
+    }
+}
