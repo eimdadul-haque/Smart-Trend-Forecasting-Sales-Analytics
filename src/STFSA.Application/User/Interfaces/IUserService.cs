0f16d5cf1318fff9056bf001e9f92619b10cbfde
DESKTOP-375QGR3\Eimdadul Haque <eimdadulhaque@gmail.com>
2025-07-11 19:31:50 +0600

    User Service added

---
+++ src/STFSA.Application/User/Interfaces/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using STFSA.Application.User.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STFSA.Application.User.Interfaces
+{
+    public interface IUserService
+    {
+        Task<ProfileDto> GetCurrentUserInfo();
+        Task<ProfileDto> UpdateProfileInfo(ProfileDto input);
+        Task<List<ProfileDto>> GetAllUsers();
+        Task<ProfileDto> ChangeUserRole(int id, string newRole);
+    }
+}
---
+++ src/STFSA.Application/User/Services/UserService.cs
@@ -0,0 +1,33 @@
+﻿using STFSA.Application.User.DTOs;
+using STFSA.Application.User.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STFSA.Application.User.Services
+{
+    public class UserService : IUserService
+    {
+        public async Task<ProfileDto> GetCurrentUserInfo()
+        {
+            return new ProfileDto();
+        }
+
+        public async Task<ProfileDto> UpdateProfileInfo(ProfileDto input)
+        {
+            return new ProfileDto();
+        }
+
+        public async Task<List<ProfileDto>> GetAllUsers()
+        {
+            return new List<ProfileDto>();
+        }
+
+        public async Task<ProfileDto> ChangeUserRole(int id, string newRole)
+        {
+            return new ProfileDto();
+        }
+    }
+}
