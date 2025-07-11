aa4c97b2979a7e59ae0d5a9164c6e8f23d2d42d0
DESKTOP-375QGR3\Eimdadul Haque <eimdadulhaque@gmail.com>
2025-07-11 20:01:15 +0600

    Sale Dataset Service added

---
+++ src/STFSA.Application/SaleDataset/Interfaces/ISaleDatasetService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STFSA.Application.SaleDataset.Interfaces
+{
+    public interface ISaleDatasetService
+    {
+        Task Get();
+        Task UploadDataset(IFormFile file);
+        Task GetDatasetDetails(int id);
+        Task DeleteDataset(int id);
+    }
+}
---
+++ src/STFSA.Application/SaleDataset/Services/SaleDatasetService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using STFSA.Application.SaleDataset.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STFSA.Application.SaleDataset.Services
+{
+    public class SaleDatasetService : ISaleDatasetService
+    {
+        public SaleDatasetService() { }
+
+        public async Task Get()
+        {
+        }
+
+        public async Task UploadDataset(IFormFile file)
+        {
+
+        }
+
+        public async Task GetDatasetDetails(int id)
+        {
+
+        }
+
+        public async Task DeleteDataset(int id)
+        {
+
+        }
+    }
+}
