11909234634c99b9e07e55414dd7903326b6dfb4
DESKTOP-375QGR3\Eimdadul Haque <eimdadulhaque@gmail.com>
2025-07-13 22:19:44 +0600

    Recommendation api added

--- src/STFSA.API/Controllers/RecommendationController.cs
+++ src/STFSA.API/Controllers/RecommendationController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using STFSA.Domain.Entities;
+using System.Buffers.Text;
+using System.Data;
 
 namespace STFSA.API.Controllers
 {
@@ -6,5 +9,18 @@ namespace STFSA.API.Controllers
     [Route("api/v{version:apiVersion}/[controller]")]
     public class RecommendationController : ControllerBase
     {
+        public RecommendationController() { }
+
+        [HttpGet("{datasetId}/recommendations")]
+        public IActionResult GetRecommendations(string datasetId)
+        {
+            return Ok(new { Message = "Recommendations fetched successfully", DatasetId = datasetId });
+        }
+
+        [HttpPost("{datasetId}/recommendations/run")]
+        public IActionResult RunRecommendations(string datasetId)
+        {
+            return Ok(new { Message = "Recommendation engine run successfully", DatasetId = datasetId });
+        }
     }
 }
