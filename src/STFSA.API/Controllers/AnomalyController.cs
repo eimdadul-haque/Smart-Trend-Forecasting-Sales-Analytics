e52fc9227be3804a2ad28ec1f57c7c9dcd977a12
DESKTOP-375QGR3\Eimdadul Haque <eimdadulhaque@gmail.com>
2025-07-13 22:17:24 +0600

    Anomaly api added

--- src/STFSA.API/Controllers/AnomalyController.cs
+++ src/STFSA.API/Controllers/AnomalyController.cs
@@ -1,10 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Data;
 
 namespace STFSA.API.Controllers
 {
     [ApiController]
-    [Route("api/v{version:apiVersion}/[controller]")]
+    [Route("api/v{version:apiVersion}/anomaly")]
     public class AnomalyController : ControllerBase
     {
+        public AnomalyController() { }
+
+        [HttpGet("{datasetId}/anomalies")]
+        public async Task<IActionResult> GetAnomaliesAsync(string datasetId)
+        {
+            return Ok();
+        }
+
+        [HttpPost("{datasetId}/anomalies/run")]
+        public async Task<IActionResult> RunAnomalyDetectionAsync(string datasetId, bool sync = false)
+        {
+            return Ok();
+        }
+
+        [HttpGet("anomalies/{id}")]
+        public async Task<IActionResult> GetAnomalyByIdAsync(string id)
+        {
+            return Ok();
+        }
     }
-}
+}  
