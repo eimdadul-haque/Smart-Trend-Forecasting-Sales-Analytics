80b05e0e28f327e8c824ff9a2acd2d683c0c8167
DESKTOP-375QGR3\Eimdadul Haque <eimdadulhaque@gmail.com>
2025-07-13 22:14:21 +0600

    Forecasting api added

--- src/STFSA.API/Controllers/ForecastingController.cs
+++ src/STFSA.API/Controllers/ForecastingController.cs
@@ -1,10 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.Extensions.Hosting;
+using System.Data;
 
 namespace STFSA.API.Controllers
 {
     [ApiController]
-    [Route("api/v{version:apiVersion}/[controller]")]
+    [Route("api/v{version:apiVersion}/forecasting")]
     public class ForecastingController : ControllerBase
     {
+        public ForecastingController() { }
+
+        [HttpPost("{datasetId}/run")]
+        public async Task<IActionResult> RunForecastAsync(string datasetId, bool sync = false)
+        {
+            return Ok();
+        }
+
+        [HttpGet("{datasetId}/latest")]
+        public async Task<IActionResult> GetLatestForecastAsync(string datasetId)
+        {
+            return Ok();
+        }
+
+        [HttpGet("{datasetId}/history")]
+        public async Task<IActionResult> GetForecastHistoryAsync(string datasetId)
+        {
+            return Ok();
+        }
+
+        [HttpGet("{forecastId}")]
+        public async Task<IActionResult> GetForecastByIdAsync(string forecastId)
+        {
+            return Ok();
+        }
     }
 }
