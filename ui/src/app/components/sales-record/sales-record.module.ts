13ac03a5200af466083860fa3d2076fc0d08ceef
DESKTOP-375QGR3\Eimdadul Haque <eimdadulhaque@gmail.com>
2025-07-16 20:28:32 +0600

    sale record module added

---
+++ ui/src/app/components/sales-record/sales-record.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SalesRecordRoutingModule } from './sales-record-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SalesRecordRoutingModule
+  ]
+})
+export class SalesRecordModule { }
