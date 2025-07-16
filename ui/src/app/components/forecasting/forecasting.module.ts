39892e2a02c7043fcf7776b5cae96937015814de
DESKTOP-375QGR3\Eimdadul Haque <eimdadulhaque@gmail.com>
2025-07-16 20:27:16 +0600

    forecasting module added

---
+++ ui/src/app/components/forecasting/forecasting.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ForecastingRoutingModule } from './forecasting-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ForecastingRoutingModule
+  ]
+})
+export class ForecastingModule { }
