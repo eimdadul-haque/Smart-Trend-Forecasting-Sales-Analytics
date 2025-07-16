a936732c45260bbed36b519ae6362be0baa01629
DESKTOP-375QGR3\Eimdadul Haque <eimdadulhaque@gmail.com>
2025-07-17 05:52:35 +0600

    login component update

--- ui/src/app/components/auth/login/login.component.ts
+++ ui/src/app/components/auth/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../../services/auth.service';
+import { Login } from '../../../models/auth';
 
 @Component({
   selector: 'app-login',
@@ -7,5 +11,43 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  submitted = false;
+  showPassword = false;
+  isLoading = false;
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private authService: AuthService) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      rememberMe: [false]
+    });
+  }
+
+  get f() { return this.loginForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    var login = {
+      userName: this.loginForm.get('email')?.value,
+      password: this.loginForm.get('password')?.value
+    } as Login;
+
+    this.isLoading = true;
+    this.authService.login(login).subscribe(response => {
+      this.isLoading = false;
+    });
+  }
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
 }
