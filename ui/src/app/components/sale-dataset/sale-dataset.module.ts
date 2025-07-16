8d8d6915b21e36aacb869125ac59a0908a250980
DESKTOP-375QGR3\Eimdadul Haque <eimdadulhaque@gmail.com>
2025-07-16 20:28:13 +0600

    sale dataset module added

---
+++ ui/src/app/components/sale-dataset/sale-dataset.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SaleDatasetRoutingModule } from './sale-dataset-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SaleDatasetRoutingModule
+  ]
+})
+export class SaleDatasetModule { }
