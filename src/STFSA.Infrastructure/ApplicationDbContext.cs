cac872f591129d2aa7d8360dd831706c9d2843d8
DESKTOP-375QGR3\Eimdadul Haque <eimdadulhaque@gmail.com>
2025-07-17 10:56:34 +0600

    add table added to database

--- src/STFSA.Infrastructure/ApplicationDbContext.cs
+++ src/STFSA.Infrastructure/ApplicationDbContext.cs
@@ -7,5 +7,12 @@ namespace STFSA.Infrastructure
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options) { }
+
+        public DbSet<AnomalyDetection> AnomalyDetections { get; set; }
+        public DbSet<ForecastResult> ForecastResults { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+        public DbSet<Recommendation> Recommendations { get; set; }
+        public DbSet<SalesRecord> SalesRecords { get; set; }
+        public DbSet<SalesDataset> SalesDatasets { get; set; }
     }
 }
