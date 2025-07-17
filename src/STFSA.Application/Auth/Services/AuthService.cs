5ed03c6223cbbb2d4f42ef93544bdf133582a800
DESKTOP-375QGR3\Eimdadul Haque <eimdadulhaque@gmail.com>
2025-07-17 09:18:58 +0600

    register method added

--- src/STFSA.Application/Auth/Services/AuthService.cs
+++ src/STFSA.Application/Auth/Services/AuthService.cs
@@ -1,5 +1,7 @@
-﻿using STFSA.Application.Auth.DTOs;
+﻿using Microsoft.AspNetCore.Identity;
+using STFSA.Application.Auth.DTOs;
 using STFSA.Application.Auth.Interfaces;
+using STFSA.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,13 +12,31 @@ namespace STFSA.Application.Auth.Services
 {
     public class AuthService : IAuthService
     {
-        public AuthService()
+        private readonly UserManager<ApplicationUser> _userManager;
+        public AuthService(
+            UserManager<ApplicationUser> userManager)
         {
+            _userManager = userManager;
         }
 
         public async Task<bool> register(RegisterDto input)
         {
-            return true;
+
+            var user = new ApplicationUser
+            {
+                FirstName = input.FirstName,
+                LastName = input.LastName,
+                Email = input.Email,
+                UserName = $"{input.FirstName.ToLower()}_{input.LastName.ToLower()}",
+                PhoneNumber = input.PhoneNumber
+            };
+
+            var result = await _userManager.CreateAsync(user, input.Password);
+
+            if (result.Succeeded)
+                return true;
+            else
+                return false;
         }
 
         public async Task<string> login(LoginDto input)
