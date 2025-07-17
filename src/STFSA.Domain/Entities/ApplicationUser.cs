b6b6f913c965cc069c26fd81a2167261d4044a0f
DESKTOP-375QGR3\Eimdadul Haque <eimdadulhaque@gmail.com>
2025-07-17 12:21:07 +0600

    user entity update

--- src/STFSA.Domain/Entities/ApplicationUser.cs
+++ src/STFSA.Domain/Entities/ApplicationUser.cs
@@ -11,5 +11,6 @@ namespace STFSA.Domain.Entities
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public bool AcceptTerms { get; set; }
     }
 }
