b0f996a2d01f3f50bb5e564b3da11ff840f57ea6
DESKTOP-375QGR3\Eimdadul Haque <eimdadulhaque@gmail.com>
2025-07-17 12:20:02 +0600

    new migration added

--- src/STFSA.Infrastructure/Migrations/20250717055149_AcceptTerms_Added_In_User_table.Designer.cs
+++ src/STFSA.Infrastructure/Migrations/20250717055149_AcceptTerms_Added_In_User_table.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using STFSA.Infrastructure;
 
@@ -11,9 +12,11 @@ using STFSA.Infrastructure;
 namespace STFSA.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250717055149_AcceptTerms_Added_In_User_table")]
+    partial class AcceptTerms_Added_In_User_table
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/STFSA.Infrastructure/Migrations/20250717055149_AcceptTerms_Added_In_User_table.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace STFSA.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AcceptTerms_Added_In_User_table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "AcceptTerms",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AcceptTerms",
+                table: "AspNetUsers");
+        }
+    }
+}
