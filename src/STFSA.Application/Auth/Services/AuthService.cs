b5e456164aac764e29329519fa37865a9c2ec1b2
DESKTOP-375QGR3\Eimdadul Haque <eimdadulhaque@gmail.com>
2025-07-18 07:43:38 +0600

    token method added

--- src/STFSA.Application/Auth/Services/AuthService.cs
+++ src/STFSA.Application/Auth/Services/AuthService.cs
@@ -1,22 +1,29 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using STFSA.Application.Auth.DTOs;
 using STFSA.Application.Auth.Interfaces;
 using STFSA.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace STFSA.Application.Auth.Services
 {
     public class AuthService : IAuthService
     {
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly IConfiguration _configuration;
+
         public AuthService(
-            UserManager<ApplicationUser> userManager)
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            IConfiguration configuration)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
+            _configuration = configuration;
         }
 
         public async Task<bool> Register(RegisterDto input)
@@ -42,7 +49,44 @@ namespace STFSA.Application.Auth.Services
 
         public async Task<string> Login(LoginDto input)
         {
-            return "dummy_token";
+            var user = await _userManager
+                .FindByEmailAsync(input.Email);
+
+            if (user is null)
+                throw new UnauthorizedAccessException("User not found.");
+
+            var result = await _signInManager
+                .PasswordSignInAsync(user.UserName, input.Password, input.RememberMe, false);
+
+            if (result.Succeeded)
+            {
+                return GenerateJwtToken(user.Email);
+            }
+            else
+            {
+                throw new UnauthorizedAccessException("Invalid login attempt.");
+            }
+        }
+
+        private string GenerateJwtToken(string email)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.ASCII
+                .GetBytes(_configuration["JWT:IssuerSigningKey"]));
+
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["JWT:Issuer"],
+                audience: _configuration["JWT:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256));
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
     }
 }
