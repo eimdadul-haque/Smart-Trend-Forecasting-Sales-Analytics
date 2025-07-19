e68a33bd50176bcff6c2dba8df8f55ad5949cc25
DESKTOP-375QGR3\Eimdadul Haque <eimdadulhaque@gmail.com>
2025-07-19 20:59:54 +0600

    app componet fix

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -5,14 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { SidebarComponent } from './components/reusable/sidebar/sidebar.component';
-import { AuthenticatedLayoutComponent } from './components/auth/authenticated-layout/authenticated-layout.component';
-
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent,
-    AuthenticatedLayoutComponent
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
