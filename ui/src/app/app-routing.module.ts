ce6acc122828515c1262bb03561e325cfaf1a61e
DESKTOP-375QGR3\Eimdadul Haque <eimdadulhaque@gmail.com>
2025-07-19 20:58:05 +0600

    route module update

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -1,29 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './authGuard/AuthGuard';
-import { LandingComponent } from './components/home/landing/landing.component';
 
 const routes: Routes = [
-  // {
-  //   path: '',
-  //   loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule),
-  //   pathMatch: 'full'
-  // },
+  {
+    path: '',
+    loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule),
+    pathMatch: 'full'
+  },
   {
     path: 'auth',
     loadChildren: () => import('./components/auth/auth.module').then(m => m.AuthModule)
   },
   {
-    path: '',
-    component: LandingComponent,
-    canActivate: [AuthGuard],
-    children: [
-      {
-        path: 'visual',
-        loadChildren: () => import('./components/visual/visual.module').then(m => m.VisualModule),
-      }
-    ]
-  },
+    path: 'visual',
+    loadChildren: () => import('./components/visual/visual.module').then(m => m.VisualModule),
+    canActivate: [AuthGuard]
+  }
 ];
 
 @NgModule({
