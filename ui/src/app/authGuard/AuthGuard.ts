a276bac40c59bf5fc553af27a4b64cbc80620944
DESKTOP-375QGR3\Eimdadul Haque <eimdadulhaque@gmail.com>
2025-07-19 21:02:08 +0600

    app fully fix

--- ui/src/app/authGuard/AuthGuard.ts
+++ ui/src/app/authGuard/AuthGuard.ts
@@ -15,7 +15,7 @@ export class AuthGuard implements CanActivate {
     if (this.authService.getToken()) {
       return true;
     } else {
-      this.router.navigate(['/login']);
+      this.router.navigate(['auth/login']);
       return false;
     }
   }
