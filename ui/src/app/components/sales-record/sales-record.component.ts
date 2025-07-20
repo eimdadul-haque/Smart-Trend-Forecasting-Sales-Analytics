699df57427f3fca49765c7d67915f4627117bf04
DESKTOP-375QGR3\Eimdadul Haque <eimdadulhaque@gmail.com>
2025-07-20 18:11:39 +0600

    sales-record component added

---
+++ ui/src/app/components/sales-record/sales-record.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sales-record',
+  standalone: false,
+  templateUrl: './sales-record.component.html',
+  styleUrl: './sales-record.component.scss'
+})
+export class SalesRecordComponent {
+
+}
--- ui/src/app/components/sales-record/sales-record.module.ts
+++ ui/src/app/components/sales-record/sales-record.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SalesRecordRoutingModule } from './sales-record-routing.module';
+import { SalesRecordComponent } from './sales-record.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SalesRecordComponent
+  ],
   imports: [
     CommonModule,
     SalesRecordRoutingModule
