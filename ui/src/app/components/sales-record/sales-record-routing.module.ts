1c580266f248c1e1c0f93665b8b5d96895780105
DESKTOP-375QGR3\Eimdadul Haque <eimdadulhaque@gmail.com>
2025-07-20 18:19:02 +0600

    route added

--- ui/src/app/components/sales-record/sales-record-routing.module.ts
+++ ui/src/app/components/sales-record/sales-record-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SalesRecordComponent } from './sales-record.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component:   SalesRecordComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
